129d99ea48cbb610c5987b9f88668e70d0ea3bf8
Piotr Gankiewicz <spetz90@gmail.com>
2016-12-08 08:01:58 +0100

    Shared project

---
+++ Warden.Services.Users.Shared/Events/SignedOut.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Warden.Common.Events;
+
+namespace Warden.Services.Users.Shared.Events
+{
+    public class SignedOut : IEvent
+    {
+        public Guid RequestId { get; }
+        public string UserId { get; }
+        public Guid SessionId { get; }
+
+        protected SignedOut()
+        {
+        }
+
+        public SignedOut(Guid requestId, string userId, Guid sessionId)
+        {
+            RequestId = requestId;
+            UserId = userId;
+            SessionId = sessionId;
+        }
+    }
+}
