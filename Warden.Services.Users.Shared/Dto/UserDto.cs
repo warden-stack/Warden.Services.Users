3a6d7f217e2fbde007d9db7f9555488001062fcd
Piotr Gankiewicz <spetz90@gmail.com>
2016-12-17 15:40:23 +0100

    User dto

--- Warden.Services.Users.Shared/Dto/UserDto.cs
+++ Warden.Services.Users.Shared/Dto/UserDto.cs
@@ -7,6 +7,7 @@ namespace Warden.Services.Users.Shared.Dto
         public Guid Id { get; set; }
         public string UserId { get; set; }
         public string Email { get; set; }
+        public string Name { get; set; }
         public string Provider { get; set; }
         public string Role { get; set; }
         public string State { get; set; }
