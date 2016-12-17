961180ef8ec0827d54f694ca12780302593e45eb
Piotr Gankiewicz <spetz90@gmail.com>
2016-12-17 11:28:30 +0100

    API key dto

---
+++ Warden.Services.Users.Shared/Dto/ApiKeyDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Warden.Services.Users.Shared.Dto
+{
+    public class ApiKeyDto
+    {
+        public Guid Id { get; set; }
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string UserId { get; set; }
+    }
+}
