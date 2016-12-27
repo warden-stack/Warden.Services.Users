da2c55a0f3bc8fc43401fb3b49fac73a3f9e5609
Piotr Gankiewicz <spetz90@gmail.com>
2016-12-27 21:58:31 +0100

    API keys endpoint

--- Warden.Services.Users/Modules/ApiKeyModule.cs
+++ Warden.Services.Users/Modules/ApiKeyModule.cs
@@ -1,4 +1,5 @@
-﻿using Warden.Services.Users.Domain;
+﻿using AutoMapper;
+using Warden.Services.Users.Domain;
 using Warden.Services.Users.Queries;
 using Warden.Services.Users.Services;
 using Warden.Services.Users.Shared.Dto;
@@ -7,8 +8,13 @@ namespace Warden.Services.Users.Modules
 {
     public class ApiKeyModule : ModuleBase
     {
-        public ApiKeyModule(IApiKeyService apiKeyService)
+        public ApiKeyModule(IMapper mapper, IApiKeyService apiKeyService) : base(mapper)
         {
+            Get("api-keys", async args => await FetchCollection<BrowseApiKeys, ApiKey>
+                (async x => await apiKeyService.BrowseAsync(x))
+                .MapTo<ApiKeyDto>()
+                .HandleAsync());  
+
             Get("users/{userId}/api-keys", async args => await FetchCollection<BrowseApiKeys, ApiKey>
                 (async x => await apiKeyService.BrowseAsync(x))
                 .MapTo<ApiKeyDto>()
