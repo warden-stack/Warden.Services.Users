b3162faf6f433d84ca4fe2ce5962f7b24ce3be77
spetz <spetz90@gmail.com>
2017-02-07 12:34:56 +0100

    Updated project.json packages and NLog configuration.

--- Warden.Services.Users/Startup.cs
+++ Warden.Services.Users/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Nancy.Owin;
 using NLog.Extensions.Logging;
+using NLog.Web;
 using Warden.Services.Users.Framework;
 
 namespace Warden.Services.Users
